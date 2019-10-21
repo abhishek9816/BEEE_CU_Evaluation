9040fb987d3ab81ade4499397cb1e237d1c31ae7
abhishek9816 <55178986+abhishek9816@users.noreply.github.com>
2019-10-21 23:51:26 +0530

    Add files via upload

---
+++ 2 switch 2 LED.ino
@@ -0,0 +1,20 @@
+int sensorPin = A0;   // select the input pin for ldr
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  pinMode(2, OUTPUT); //pin connected to the led
+  Serial.begin(9600); //sets serial port for communication
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);    
+  Serial.println(sensorValue); //prints the values coming from the sensor on the screen
+  
+  if(sensorValue < 900) //setting a threshold value
+  digitalWrite(2,HIGH); //turn led ON
+  
+  else digitalWrite(2,LOW); //turn led OFF
+  
+  delay(100);                  
+}
